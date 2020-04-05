72c76089153b2853c5780c883de3891ea9700c3d
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-04-05 00:19:58 -0300

    add UserService

---
+++ src/app/services/UserService.js
@@ -0,0 +1,22 @@
+import User from '../models/User';
+
+const UserService = {
+  async create({ name, email, password_hash }) {
+    const user = await User.create({
+      name,
+      email,
+      password_hash,
+    });
+
+    return user;
+  },
+
+  async userExists(email) {
+    const userExists = await User.findOne({
+      where: { email },
+    });
+    return userExists;
+  },
+};
+
+export default UserService;
