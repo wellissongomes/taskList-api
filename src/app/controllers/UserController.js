29fd2382a935282db2cb4c3a73ab9b9af0f07515
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-04-09 16:05:28 -0300

    criando metodo update no UserController

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,7 @@
 import UserService from '../services/UserService';
 
+import { validField } from '../util/util';
+
 const UserController = {
   async store(req, res) {
     const { email: user_email } = req.body;
@@ -14,7 +16,25 @@ const UserController = {
   },
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    const userById = await UserService.getUserById(req.userId);
+    const userByEmail = await UserService.getUserByEmail(email);
+
+    const userAlreadyExists = !(email !== userById.email && userByEmail);
+    validField(userAlreadyExists, res, 400, {
+      error: 'Já existe um usuário com esse email. Tente novamente.',
+    });
+
+    const incorrectPassword = !(
+      oldPassword && !(await userById.checkPassword(oldPassword))
+    );
+    validField(incorrectPassword, res, 401, {
+      error: 'Senha incorreta. Tente novamente',
+    });
+
+    const { id, name } = await userById.update(req.body);
+    return res.json({ id, name, email });
   },
 };
 
