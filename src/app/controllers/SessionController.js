c27eae1f98a587634b63bc0462497670073635ea
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-04-08 21:21:14 -0300

    add metodo store em SessionController

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,36 @@
+import jwt from 'jsonwebtoken';
+import UserService from '../services/UserService';
+
+import authConfig from '../../config/auth';
+
+const SessionController = {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await UserService.userExists(email);
+    if (!user) {
+      return res.status(401).json({ error: 'Email inexistente.' });
+    }
+
+    const correctPassword = await user.checkPassword(password);
+    if (!correctPassword) {
+      return res.status(401).json({ error: 'Senha incorreta.' });
+    }
+
+    const { id, name } = user;
+    const { secret, expiresIn } = authConfig;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, secret, {
+        expiresIn,
+      }),
+    });
+  },
+};
+
+export default SessionController;
