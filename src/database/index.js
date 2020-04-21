69d1c9a8d636f03235b3b9acf02bed62535cddef
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-04-21 20:47:51 -0300

    adding Task model on database init

--- src/database/index.js
+++ src/database/index.js
@@ -2,13 +2,17 @@ import Sequelize from 'sequelize';
 import dbConfig from '../config/database';
 
 import User from '../app/models/User';
+import Task from '../app/models/Task';
 
-const models = [User];
+const models = [User, Task];
 
 const Database = {
   init() {
     const connection = new Sequelize(dbConfig);
     models.map((model) => model.init(connection));
+    models.map(
+      (model) => model.associate && model.associate(connection.models)
+    );
   },
 };
 
